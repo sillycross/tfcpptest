f3f2578cc6edb56a0d8b69c987724aff0c017882
Haoran Xu <haoranxu510@gmail.com>
2019-10-02 19:29:55 -0700

    add sanity tf test

---
+++ test_sanity.cpp
@@ -0,0 +1,25 @@
+#include <tensorflow/core/platform/env.h>
+#include <tensorflow/core/public/session.h>
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "common.h"
+
+using namespace std;
+using namespace tensorflow;
+
+TEST(TFCpp, Sanity)
+{
+    Session* session;
+    Status status = NewSession(SessionOptions(), &session);
+    if (!status.ok()) {
+        cout << status.ToString() << "\n";
+        ReleaseAssert(false);
+    }
+    else
+    {
+    	cout << "Session successfully created.\n";
+    }
+} 
